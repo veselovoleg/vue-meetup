80d05a82bd0b083ddfe46517e440a79ce5b52470
Veselov.Oleg <veselov.oleg@wildberries.ru>
2018-09-08 13:35:22 +0300

    Colors

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,39 @@
 <template>
   <v-app>
-    
+    <v-navigation-drawer v-model="sideNav" fixed>
+      <v-list>
+        <v-list-tile @click.prevent>
+          <v-list-tile-action>
+            <v-icon>people</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-title>Meetups</v-list-tile-title>
+        </v-list-tile>
+    </v-list>
+    </v-navigation-drawer>
+    <v-toolbar dark class="primary">
+      <v-toolbar-side-icon @click="sideNav = !sideNav" 
+      class="hidden-sm-and-up"></v-toolbar-side-icon>
+      <v-toolbar-title>DevMeetup</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn flat>
+          <v-icon left>account_balance</v-icon>
+          Meetups
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <main>
+
+    </main>
   </v-app>
 </template>
 
 <script>
 export default {
   data() {
-    return {};
+    return {
+      sideNav: null
+    };
   },
   name: "App"
 };
--- src/main.js
+++ src/main.js
@@ -4,6 +4,20 @@ import router from './router'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 
+import colors from 'vuetify/es5/util/colors'
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: colors.red.darken1,
+    secondary: '#424242',
+    accent: '#82B1FF',
+    error: '#FF5252',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FFC107'
+  }
+})
+
 Vue.use(Vuetify)
 
 Vue.config.productionTip = false
